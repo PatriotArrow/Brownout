2d4e100a6e7c693d2619b98b92c83bd9087bd2b3
thicc_boi <jinurajukl@gmail.com>
2021-12-05 12:14:53 +0530

    updated js and html

--- argon-dashboard-master/index.js
+++ argon-dashboard-master/index.js
@@ -1,5 +1,5 @@
 
-// consumer type 1. residential 2.commercial 3. industrial
+
 //section (01 - 99)
 //phase = (1- 2)
 // total consumption , payable amount
@@ -15,10 +15,17 @@
 
 
 // }
-
+function randomNumber(min, max) { 
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function phase() { 
     var phase = 1;
+    if(consumerType== 2 || consumerType==3){
+        return 3;
+    }
     min = Math.ceil(1);
     max = Math.floor(3);
     phase = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -39,8 +46,31 @@ function unitPrice(){
 
 
 }
-function m(min, max) { 
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-} 
+function consumerType() { // consumer type 1. residential 2.commercial 3. industrial
+    min = Math.ceil(1);
+    max = Math.floor(3); 
+    var consumerType =  Math.floor(Math.random() * (max - min + 1)) + min;
+    if (consumerType== 1){
+        return "Residential"
+    }else if (consumerType == 2){
+        return "Commercial";
+    }else return "Industrial";
+}
+
+function unitsConsumed(){
+
+    if (consumerType== 1){
+        var unitsConsumed=randomNumber(700,900);
+    }
+    else if (consumerType==2){
+        var unitsConsumed= randomNumber(800,1000);        
+    }
+    else unitsConsumed = randomNumber(1200,3000);
+    return unitsConsumed;
+}
+
+function amountDue(){
+    amountDue = unitsConsumed * unitsConsumed;
+    return amountDue;
+}
+
